390a4b0dc3dcfeb364c25981fa24554a9d01b476
M. Drew LaMar <mdlama@wm.edu>
2017-08-19 15:26:06 -0400

    Added working example that creates a graph with two nodes and one edge.

--- inst/htmlwidgets/mxgraphr.js
+++ inst/htmlwidgets/mxgraphr.js
@@ -6,21 +6,43 @@ HTMLWidgets.widget({
 
   factory: function(el, width, height) {
 
-    // TODO: define shared variables for this instance
+    // Variables to keep track of the state of the widget
+    var graph = null;
+    var parent = null;
 
     return {
 
       renderValue: function(x) {
 
-        // TODO: code to render the widget, e.g.
-        el.innerText = x.message;
+        if (graph === null) {
+          graph = new mxGraph(document.getElementById(el.id));
+          parent = graph.getDefaultParent();
 
+          // Adds cells to the model in a single step
+				  graph.getModel().beginUpdate();
+				  try
+				  {
+					  var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30, 'rounded;strokeColor=red;fillColor=green');
+					  var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
+					  var e1 = graph.insertEdge(parent, null, '', v1, v2);
+				  }
+				  finally
+				  {
+					  // Updates the display
+					  graph.getModel().endUpdate();
+				  }
+        }
       },
 
       resize: function(width, height) {
 
         // TODO: code to re-render the widget with a new size
 
+      },
+
+      // Give access to grid if anyone needs it on the outside
+      getGraph: function() {
+        return graph;
       }
 
     };
