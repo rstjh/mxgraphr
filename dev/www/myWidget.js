c0f1938b25684a08b1ca6dd87e9a61b248b0a11c
rstjh <you@example.com>
2020-03-19 21:04:01 +0000

    Further updates

--- dev/www/myWidget.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.grid-stack').on('click', '.gs-remove-handle', function() {
-    var el = $(this).parents('.grid-stack-item');
-    el.fadeOut(200, function(){
-      $('.grid-stack').data('gridstack').removeWidget(el, true);
-    });
-  });
-});
